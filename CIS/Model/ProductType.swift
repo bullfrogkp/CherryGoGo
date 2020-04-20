58a6c9d8c885f8f1268a4e785816f3c6ee84ef1b
pank <pqhitpan@gmail.com>
2020-04-20 00:31:36 -0400

    product type

---
+++ CIS/Model/ProductType.swift
@@ -0,0 +1,19 @@
+//
+//  Product.swift
+//  CIS
+//
+//  Created by Kevin Pan on 2020-04-20.
+//  Copyright © 2020 Kevin Pan. All rights reserved.
+//
+
+import Foundation
+
+class ProductType {
+    var name: String
+    var brand: String
+    
+    init(name: String, brand: String) {
+        self.name = name
+        self.brand = brand
+    }
+}
