f4692201a39469a9402f644c77492bf18f71ca08
pank <pqhitpan@gmail.com>
2020-05-26 15:56:52 -0400

    fff

---
+++ CIS/View/InStockContactTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  InStockContactTableViewCell.swift
+//  CIS
+//
+//  Created by Kevin Pan on 2020-05-26.
+//  Copyright © 2020 Kevin Pan. All rights reserved.
+//
+
+import UIKit
+
+class InStockContactTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ CIS/View/InStockItemTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  InStockItemTableViewCell.swift
+//  CIS
+//
+//  Created by Kevin Pan on 2020-05-26.
+//  Copyright © 2020 Kevin Pan. All rights reserved.
+//
+
+import UIKit
+
+class InStockItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
